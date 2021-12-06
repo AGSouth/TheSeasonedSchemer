60179d27385dc5a210c6332cad7662a41d79e095
AGSouth <joe.maline@applegrovesouth.com>
2021-12-06 12:05:39 -0500

    Added Simply Scheme for this

--- TheSeasonedSchemer.rkt
+++ TheSeasonedSchemer.rkt
@@ -1,7 +1,15 @@
-#lang racket
+;; #lang racket
+#lang scheme
 
 (require "../TheLittleSchemer/TheLittleSchemer.rkt")
-# Chapter 11 of The Seasoned Schemer
+
+(define Y
+  (lambda (le)
+    ((lambda (f) (f f))
+     (lambda (f)
+       (le (lambda (x) ((f f) x)))))))
+
+;; Chapter 11 of The Seasoned Schemer
 (define scramble-b
   (lambda (tup rev-pre)
     (cond
@@ -16,4 +24,20 @@
    (lambda (tup)
      (scramble-b tup (quote ()))))
 
-    
+ (define multirember
+    (lambda (a lat)
+      ((Y (lambda (mr)
+            (lambda (lat)
+              (cond
+                 ((null? lat) (quote ()))
+                 ((eq? a (car lat))
+                    (mr (cdr lat)))
+                  (else (cons (car lat)
+                               (mr (cdr lat))))))))
+              lat)))
+
+;; from Simply Scheme trying to understand lambda
+;; so that I can understand Y combinators
+
+(define (same-arg-twice fn)
+  (lambda (arg) (fn arg arg)))
